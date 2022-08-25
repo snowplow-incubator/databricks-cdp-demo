10aa6b201f53bef4dccdebe38b26e1c09ab0cbc4
georgewoodhead <georgewoodhead>
2022-08-25 19:15:29 +0000

    update introduction with braze

--- 00_Overview.sql
+++ 00_Overview.sql
@@ -6,11 +6,10 @@
 -- MAGIC 
 -- MAGIC <img src="files/images/composable_cdp.png" width="80%">
 -- MAGIC 
--- MAGIC In these notebooks we will be exploring behavioural data collected by Snowplow's Javascript tracker from Snowplow's own [website](https://snowplowanalytics.com/). We will then model this data to make it analytics ready using dbt. The end goal will be to use this dataset to more effectivley target customers to book a demo with Snowplow's platform using Hightouch to sync to 3rd party destinations like Google Ads.
+-- MAGIC In these notebooks we will be exploring behavioural data collected by Snowplow's Javascript tracker from Snowplow's own [website](https://snowplowanalytics.com/). We will then model this data to make it analytics ready using dbt. This dataset can then be used to more effectivley target customers using Hightouch to sync to 3rd party destinations.
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC *{{show end result of Hightouch linked to Google Ads}}*
--- MAGIC 
--- MAGIC <img src="files/images/hightouch_syncs.png" width="50%">
+-- MAGIC In this demo we show how we can build user segments based on a user's web beavioural data and sync these to Braze email subscription groups.
+-- MAGIC <img src="files/images/hightouch_syncs_braze.png" width="50%">
