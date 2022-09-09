dbfa178fbe49a70d8260a2d9d3f41966010b4bda
martin@snowplowanalytics.com <martin@snowplowanalytics.com>
2022-09-09 09:40:02 +0000

    updated activation

--- 03_Data Activation.sql
+++ 03_Data Activation.sql
@@ -13,43 +13,47 @@
 -- MAGIC 
 -- MAGIC We can easily connect to Hightouch from Databricks using [Partner Connect](https://dbc-dcab5385-51e3.cloud.databricks.com/partnerconnect?o=2894723222787945):
 -- MAGIC 
--- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_partner_connect.png" width="27%">
+-- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_partner_connect.png" width="25%">
 -- MAGIC 
 -- MAGIC Once setup we can see our Databricks cluster in the [Sources](https://app.hightouch.com/snowplow-yzw4c/sources) tab in Hightouch:
 -- MAGIC 
--- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_sources.png" width="30%">
+-- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_sources.png" width="25%">
 
 -- COMMAND ----------
 
 -- MAGIC %md
 -- MAGIC ## 3.2. Create the audience cohort
 -- MAGIC 
--- MAGIC Say our Marketing team want to target two user segments, *Awareness Users* and *Engagement Users*, based on their web beavioural data. They need these users available in the team's Marketing tool, Braze, to launch the email campaigns. We can set this up by creating two new Hightouch audiences.
+-- MAGIC Say our Marketing team want to target two user segments and execute marketing campaigns to accelerate conversion (create new MQLs) and engage new visitors with the product. 
+-- MAGIC # 
+-- MAGIC 1. **Awareness visitors**
+-- MAGIC 2. **Engagement visitors**
+-- MAGIC 
 -- MAGIC 
 -- MAGIC First we create a parent model based on all our `snowplow_web_users` table for the audiences to be built off of (see [here](https://app.hightouch.com/snowplow-yzw4c/audiences/setup/parent-models/591471)). 
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC ### Awareness Users
+-- MAGIC ### 3.2.1. Awareness Users
 -- MAGIC Create an [Awareness Users](https://app.hightouch.com/snowplow-yzw4c/audiences/591472) audience based on the following rules:
 -- MAGIC 
 -- MAGIC - First time on website
 -- MAGIC - Lower engagement (less than 60 secs engaged)
 -- MAGIC - Landed on the Snowplow homepage
 -- MAGIC 
--- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_awareness_users_audience_builder.png" width="50%">
+-- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_awareness_users_audience_builder.png" width="40%">
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC ### Engagement Users
+-- MAGIC ### 3.2.2. Engagement Users
 -- MAGIC Create an [Engagement Users](https://app.hightouch.com/snowplow-yzw4c/audiences/591474) audience based on the following rules:
 -- MAGIC 
 -- MAGIC - Returned (2 sessions)
--- MAGIC - Had intent to convert within the last 30 days
+-- MAGIC - Had intent to engage within the last 30 days
 -- MAGIC 
--- MAGIC To flag if a user had intent to convert we see if they had viewed one of the [get-started](https://snowplowanalytics.com/get-started/) pages on Snowplow's website. We can make this an Audience Event using the following query based on our `snowplow_web_page_views` table:
+-- MAGIC To flag if a user had intent to engage we see if they had viewed one of the [get-started](https://snowplowanalytics.com/get-started/) pages on Snowplow's website. We can make this an Audience Event using the following query based on our `snowplow_web_page_views` table:
 -- MAGIC 
 -- MAGIC ```sql
 -- MAGIC select 
@@ -62,16 +66,16 @@
 -- MAGIC 
 -- MAGIC After creating the event (see set up [here](https://app.hightouch.com/snowplow-yzw4c/audiences/setup/events/591234)), we need to add a direct relationship between this and our *All Users* parent model.
 -- MAGIC 
--- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_direct_relationship.png" width="50%">
+-- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_direct_relationship.png" width="35%">
 -- MAGIC 
 -- MAGIC We can now use this event as a filter when we build our Engagement Users audience:
 -- MAGIC 
--- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_engagement_users_audience_builder.png" width="50%">
+-- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_engagement_users_audience_builder.png" width="35%">
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC ### 3.3. Sync the Audiences to Braze
+-- MAGIC ## 3.3. Sync the created Audiences with Marketing channels
 -- MAGIC 
 -- MAGIC After setting up Braze as a [destination](https://app.hightouch.com/snowplow-yzw4c/destinations) in Hightouch, we can sync up our new audiences. In this case we want to sync these audiences to our *Awareness Users* and *Engagement Users* Braze subscription groups.
 -- MAGIC 
@@ -80,20 +84,20 @@
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC ## Syncs
+-- MAGIC ## 3.4. Orchestrate an auto-update of audiences
 -- MAGIC It is important that our audiences connected to third party tools like Braze are always up to date and in sync with our Snowplow web data in Databricks. 
 -- MAGIC 
 -- MAGIC We can ensure this by using the [dbt Cloud extension](https://app.hightouch.com/snowplow-yzw4c/extensions) to trigger syncs after the dbt Snowplow web model job finishes and our Gold tables are updated.
 -- MAGIC 
--- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_dbt_schedule.png" width="40%">
+-- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_dbt_schedule.png" width="30%">
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC ## Summary
+-- MAGIC ## 3.5. Summary
 -- MAGIC 
 -- MAGIC We have now finished creating our audiences and have them synced up to Braze!
 -- MAGIC 
--- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_syncs_braze.png" width="60%">
+-- MAGIC <img src="https://raw.githubusercontent.com/snowplow-incubator/databricks-cdp-demo/main/assets/hightouch_syncs_braze.png" width="35%">
 -- MAGIC 
 -- MAGIC Hightouch is now setup with all the data and events from Databricks to enable our teams to easily build new audiences based on Snowplow's rich behavioural data and sync to their needed destinations.
